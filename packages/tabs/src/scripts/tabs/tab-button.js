76775230a039d5b3a1202113c403940ba1afd5d2
keryan <keryan.sanie@gmail.com>
2020-11-10 15:24:07 +0100

    feat(tabs): implement Tabs classes - change structrure - comments

--- packages/tabs/src/scripts/tabs/tab-button.js
+++ packages/tabs/src/scripts/tabs/tab-button.js
@@ -1,10 +1,14 @@
 import { DisclosureButton } from '@frds/utilities/src/scripts';
 
+/**
+  * TabButton, extends DisclosureButton that add aria-selected, and add attributte tabindex and the focus if current button
+ */
 class TabButton extends DisclosureButton {
   apply (value) {
     super.apply(value);
     if (this.hasAttribute) {
       this.element.setAttribute('tabindex', value ? '0' : '-1');
+      if (value) this.element.focus();
     }
   }
 }
