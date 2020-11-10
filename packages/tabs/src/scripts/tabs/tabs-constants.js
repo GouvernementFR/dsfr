4a84a2d30babeee5ac57e37df04ff0a2ab125c51
keryan <keryan.sanie@gmail.com>
2020-11-10 15:24:07 +0100

    feat(tabs): implement Tabs classes - change structrure - comments

--- packages/tabs/src/scripts/tabs/tabs-constants.js
+++ packages/tabs/src/scripts/tabs/tabs-constants.js
@@ -1,1 +1,14 @@
+const TABS_SELECTOR = '.${prefix}-tabs';
 
+const PANEL_SELECTOR = '.${prefix}-tabs__panel';
+
+const TABS_LIST_SELECTOR = '.${prefix}-tabs__list';
+
+const KEY_BINDS = {
+  END: 35,
+  HOME: 36,
+  LEFT: 37,
+  RIGHT: 39
+};
+
+export { TABS_SELECTOR, KEY_BINDS, PANEL_SELECTOR, TABS_LIST_SELECTOR };
--- packages/tabs/src/scripts/tabs/tabs-group.js
+++ packages/tabs/src/scripts/tabs/tabs-group.js
@@ -1,21 +1,62 @@
 import { Tab } from './tab';
 import { Disclosure, DisclosuresGroup } from '@frds/utilities/src/scripts';
+import { KEY_BINDS, PANEL_SELECTOR, TABS_LIST_SELECTOR } from './tabs-constants';
 
-const PANEL = '.${prefix}-tabs__panel';
-
+/**
+* TabGroup is the class form 1 tabs with multiple tab-button & Tab (panel)
+*/
 class TabsGroup extends DisclosuresGroup {
   constructor (wrapper) {
     super();
     this._index = -1;
+    this.element = wrapper;
 
     console.log('build');
-    this.build(wrapper, PANEL, Disclosure.SELECT);
+    this.build(wrapper, PANEL_SELECTOR, Disclosure.SELECT);
 
     if (this.current === null) this.index = 0;
+
+    this._attachEvents();
   }
 
   disclosureFactory (element, type, selector) {
     return new Tab(element, type, selector);
+  }
+
+  _attachEvents () {
+    this.element.addEventListener('keydown', e => {
+      switch (e.keyCode) {
+        case KEY_BINDS.RIGHT:
+          // If we're at the end, go to the start
+          if (this.index < this.disclosures.length - 1) {
+            this.index++;
+          } else {
+            this.index = 0;
+          }
+          e.preventDefault();
+          break;
+
+        case KEY_BINDS.LEFT:
+          // If we're at the start, move to the end
+          if (this.index > 0) {
+            this.index--;
+          } else {
+            this.index = this.disclosures.length - 1;
+          }
+          e.preventDefault();
+          break;
+
+        case KEY_BINDS.HOME:
+          this.index = 0;
+          e.preventDefault();
+          break;
+
+        case KEY_BINDS.END:
+          this.index = this.disclosures.length - 1;
+          e.preventDefault();
+          break;
+      };
+    });
   }
 
   get index () { return this._index; }
@@ -24,6 +65,7 @@ class TabsGroup extends DisclosuresGroup {
     if (value < 0 || value >= this.disclosures.length || this._index === value) return;
     this._index = value;
     this.current = this.disclosures[value];
+    this.setPanelHeight();
   }
 
   get current () { return super.current; }
@@ -31,6 +73,11 @@ class TabsGroup extends DisclosuresGroup {
   set current (controller) {
     super.current = controller;
     this._index = this.disclosures.indexOf(controller);
+    this.setPanelHeight();
+  }
+
+  setPanelHeight () {
+    this.element.querySelector(TABS_LIST_SELECTOR).style.marginBottom = getComputedStyle(this.current.element).height;
   }
 }
 
