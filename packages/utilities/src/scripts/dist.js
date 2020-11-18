0be3a4dac2fcfc6edd0052b9470d85ff416a8d84
keryan <keryan.sanie@gmail.com>
2020-11-18 10:51:36 +0100

    feat: ajout utilitaire collapse & KeyEventListener

--- packages/utilities/src/scripts/dist.js
+++ packages/utilities/src/scripts/dist.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-new */
-import '@gouvfr/utilities/_dist.scss';
+import '@frds/utilities/_dist.scss';
 
-import { Initializer } from '@gouvfr/utilities/src/scripts/init/Initializer';
+import { Initializer } from '@frds/utilities/src/scripts/init/Initializer';
 import { Collapse } from './index';
 
 new Initializer('.${prefix}-collapse', [Collapse]);
