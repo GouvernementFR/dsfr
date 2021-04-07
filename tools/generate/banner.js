6d15f30f0dc0d1ccd30dc41751c6f5c11236f97c
lab9 <lab9.fr@gmail.com>
2021-04-07 19:53:07 +0200

    chore(global): changement de la banière des fichiers scripts et styles générés

--- tools/generate/banner.js
+++ tools/generate/banner.js
@@ -1,7 +1,7 @@
 const global = require('../../package.json');
 
 const getBanner = () => {
-  return `${global.config.namespace.toUpperCase()} v${global.version} | licence ${global.license}`;
+  return `${global.config.namespace.toUpperCase()} v${global.version} | Not for personal use`;
 };
 
 module.exports = { getBanner };
