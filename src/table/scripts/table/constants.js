c3d5af8f3ed2f83a74297a94ccec545b03bd515f
Rafaël BUCHER <rafael.bchr@gmail.com>
2021-06-21 17:28:12 +0200

    refactor(table): gestion des ombres sur les tableaux sans défilement

--- src/table/scripts/table/constants.js
+++ src/table/scripts/table/constants.js
@@ -1,7 +1,7 @@
 import api from '../../api.js';
 
 export const TABLE_SELECTOR = api.core.ns.selector('table');
-export const TABLE_NOSCROLL_SELECTOR = api.core.ns.selector('table--no-scroll');
+export const TABLE_NOSCROLL_SELECTOR = api.core.ns('table--no-scroll');
 export const LEFT = 'left';
 export const RIGHT = 'right';
 export const SHADOW_CLASS = api.core.ns('table--shadow');
