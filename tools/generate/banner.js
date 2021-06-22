1aa75d2614ca2dae931ede4760fc1381434ac11f
JC Hourdeaux <jean-charles.hourdeaux@pm.gouv.fr>
2021-06-22 19:27:21 +0200

    build: maj message bannière

--- tools/generate/banner.js
+++ tools/generate/banner.js
@@ -1,7 +1,7 @@
 const global = require('../../package.json');
 
 const getBanner = () => {
-  return `${global.config.namespace.toUpperCase()} v${global.version} | restricted use`;
+  return `${global.config.namespace.toUpperCase()} v${global.version} | utilisation restreinte (voir les conditions générales d'utilisation)`;
 };
 
 module.exports = { getBanner };
