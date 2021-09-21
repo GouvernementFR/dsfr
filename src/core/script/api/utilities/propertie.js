cbedee3d6e4a94529aae05396f2f5dc844609f8f
zellerbaptiste <zellerbaptiste@gmail.com>
2021-09-21 17:36:10 +0200

    fix(api): add doc on mixProperties method

--- src/core/script/api/utilities/propertie.js
+++ src/core/script/api/utilities/propertie.js
@@ -1,3 +1,24 @@
+/**
+ * Copy properties from multiple sources including accessors.
+ *
+ * @param {...objects} Multiple objects
+ * @return {object} A new object
+ *
+ * @example
+ *
+ *     const obj1 = {
+ *        key: 'value'
+ *     };
+ *     const obj2 = {
+ *        get function01 () {
+ *          return a-value;
+ *        } 
+ *        set function01 () {
+ *          return a-value;
+ *        }
+ *     };
+ *     mixProperties(obj1, obj2)
+ */
 const mixProperties = (...sources) => {
   const result = {};
   for (const source of sources) {
