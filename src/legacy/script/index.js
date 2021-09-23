ed10e142e89f99f38d8de35e3e6d4d9297a2d300
zellerbaptiste <zellerbaptiste@gmail.com>
2021-09-23 14:57:53 +0200

    fix(lagacy-browser): update polyfills imports

--- src/legacy/script/index.js
+++ src/legacy/script/index.js
@@ -7,11 +7,7 @@ import './polyfill-io/contains-polyfill.min';
 import './polyfill-io/matches-polyfill.min';
 
 import './css-vars-ponyfill/css-vars-ponyfill.min';
-// import './custom-event-polyfill/custom-event-polyfill';
-// import './contains-polyfill/contains-polyfill.min';
 // import './focus-within-polyfill/focus-within-polyfill';
-// import './element-matches-polyfill/element-matches-polyfill';
-// import './foreach-polyfill/foreach-polyfill';
 import { ObjectFitPolyfill } from './object-fit-polyfill/object-fit-polyfill';
 
 new ObjectFitPolyfill('.${prefix}-content-media__img img', '.${prefix}-card__img img', '${prefix}-responsive-vid');
