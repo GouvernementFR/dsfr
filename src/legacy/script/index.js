3f3bb2b1e6041de9d5fce91b49ceb9b25d48db95
zellerbaptiste <zellerbaptiste@gmail.com>
2021-09-24 11:23:11 +0200

    fix(legacy-browser): delete unused polyfill scripts

--- src/legacy/script/index.js
+++ src/legacy/script/index.js
@@ -6,8 +6,6 @@ import './polyfill-io/foreach-polyfill.min';
 import './polyfill-io/contains-polyfill.min';
 import './polyfill-io/matches-polyfill.min';
 
-import './css-vars-ponyfill/css-vars-ponyfill.min';
-// import './focus-within-polyfill/focus-within-polyfill';
-import { ObjectFitPolyfill } from './object-fit-polyfill/object-fit-polyfill';
+// import { ObjectFitPolyfill } from './object-fit-polyfill/object-fit-polyfill';
 
-new ObjectFitPolyfill('.${prefix}-content-media__img img', '.${prefix}-card__img img', '${prefix}-responsive-vid');
+// new ObjectFitPolyfill('.${prefix}-content-media__img img', '.${prefix}-card__img img', '${prefix}-responsive-vid');
