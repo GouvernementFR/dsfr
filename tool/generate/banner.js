5c19966686211b428d1695a6e5c2115d9c393089
keryan <keryan.sanie@gmail.com>
2021-10-14 19:17:06 +0200

    fix: licence

--- tool/generate/banner.js
+++ tool/generate/banner.js
@@ -1,7 +1,7 @@
 const global = require('../../package.json');
 
 const getBanner = () => {
-  return `${global.config.namespace.toUpperCase()} v${global.version} | SPDX-License-Identifier: MIT | License-Filename: LICENCE.md | restricted use (see terms and conditions)`;
+  return `${global.config.namespace.toUpperCase()} v${global.version} | SPDX-License-Identifier: MIT | License-Filename: LICENSE.md | restricted use (see terms and conditions)`;
 };
 
 module.exports = { getBanner };
