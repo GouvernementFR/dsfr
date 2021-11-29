5bc90f70e744308f5e91d3b3b96106c0c1f841b2
lab9 <lab9.fr@gmail.com>
2021-11-29 13:03:15 +0100

    fix(modal): attribut inert retiré

--- src/component/modal/script/modal/focus-trap.js
+++ src/component/modal/script/modal/focus-trap.js
@@ -23,6 +23,8 @@ const ordereds = [
 
 const ORDEREDS = ordereds.join();
 
+const IS_STUNNING = false;
+
 const isFocusable = (element, container) => {
   if (!(element instanceof Element)) return false;
   const style = window.getComputedStyle(element);
@@ -80,8 +82,10 @@ class FocusTrap {
     window.addEventListener('keydown', this.handling);
     document.body.addEventListener('focus', this.focusing, true);
 
-    this.stunneds = [];
-    this.stun(document.body);
+    if (IS_STUNNING) {
+      this.stunneds = [];
+      this.stun(document.body);
+    }
   }
 
   stun (node) {
@@ -178,8 +182,10 @@ class FocusTrap {
 
     this.element = null;
 
-    for (const stunned of this.stunneds) stunned.unstun();
-    this.stunneds = [];
+    if (IS_STUNNING) {
+      for (const stunned of this.stunneds) stunned.unstun();
+      this.stunneds = [];
+    }
 
     if (this.onUntrap) this.onUntrap();
   }
