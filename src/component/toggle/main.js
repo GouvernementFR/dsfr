9bc774e76f54df7782a74e6f95686749ee6f96a5
Keryan SANIE <keryan.sanie@gmail.com>
2022-01-18 12:26:06 +0100

    Update src/component/toggle/main.js
    Co-authored-by: lab9 <lab9.fr@gmail.com>

--- src/component/toggle/main.js
+++ src/component/toggle/main.js
@@ -1,5 +1,5 @@
 import api from './index.js';
 
-api.internals.register(api.toggle.ToggleSelector.LABEL_WITH_STATUS, api.toggle.ToggleStatusLabel);
+api.internals.register(api.toggle.ToggleSelector.STATUS_LABEL, api.toggle.ToggleStatusLabel);
 
 export default api;
