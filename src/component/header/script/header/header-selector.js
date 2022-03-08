e4736e12a9b0b95ae25f0ed2afc5e7d30e2dbc51
lab9 <lab9.fr@gmail.com>
2022-03-08 14:12:20 +0100

    refactor(header): intégration nouveau links et buttons

--- src/component/header/script/header/header-selector.js
+++ src/component/header/script/header/header-selector.js
@@ -4,6 +4,6 @@ export const HeaderSelector = {
   HEADER: api.internals.ns.selector('header'),
   TOOLS_LINKS: api.internals.ns.selector('header__tools-links'),
   MENU_LINKS: api.internals.ns.selector('header__menu-links'),
-  LINKS: `${api.internals.ns.selector('header__tools-links')} ${api.internals.ns.selector('links-group')}`,
+  LINKS: `${api.internals.ns.selector('header__tools-links')} ${api.internals.ns.selector('btns-group')}`,
   MODALS: `${api.internals.ns.selector('header__search')}${api.internals.ns.selector('modal')}, ${api.internals.ns.selector('header__menu')}${api.internals.ns.selector('modal')}`
 };
