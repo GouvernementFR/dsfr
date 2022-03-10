02f34158fe0c538b1b7f30a4c7b897d17367d045
lab9 <lab9.fr@gmail.com>
2022-03-10 18:37:06 +0100

    refactor(global): soulignement des liens et ajout des boutons tertiaires [DS-2022][DS-2427] (#234)

---
+++ src/core/script/api/modules/module.js
@@ -0,0 +1,14 @@
+import { Collection } from '../utilities/collection.js';
+
+class Module extends Collection {
+  constructor (type) {
+    super();
+    this.type = type;
+    this.isActive = false;
+  }
+
+  activate () {}
+  deactivate () {}
+}
+
+export { Module };
