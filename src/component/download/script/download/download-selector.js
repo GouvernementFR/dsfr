09e4cf30ef1d4835f2bb4bb072f1b5b4efbe2361
keryan <keryan.sanie@gmail.com>
2022-04-25 13:32:26 +0200

    fix(download): clean console.log

--- src/component/download/script/download/download-selector.js
+++ src/component/download/script/download/download-selector.js
@@ -1,6 +1,5 @@
 import api from '../../api.js';
 
-console.log(api.internals.ns.attr.selector('assess-file'));
 export const DownloadSelector = {
   DOWNLOAD_ASSESS_FILE: `${api.internals.ns.attr.selector('assess-file')}`,
   DOWNLOAD_DETAIL: `${api.internals.ns.selector('download__detail')}`
