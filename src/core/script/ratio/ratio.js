077f9b95d4b6e7af49e833ccd8c4987300c30bf2
keryan <keryan.sanie@gmail.com>
2022-05-11 19:57:56 +0200

    fix(core, footer, header, content): nouveau systeme de ratio

--- src/core/script/ratio/ratio.js
+++ src/core/script/ratio/ratio.js
@@ -7,9 +7,34 @@ class Ratio extends Instance {
   }
 
   init () {
-    if (!api.internals.support.supportLocalStorage()) {
+    if (!api.internals.support.supportAspectRatio()) {
+      this.ratio = 16 / 9;
+      for (const className in this.classNames) {
+        console.log(this.classNames[className], this.registration.selector.indexOf(this.classNames[className]), this.classNames[className].split('--'));
+        if (this.registration.selector.indexOf(this.classNames[className]) > 0) {
+          const ratio = this.classNames[className].split('--');
+          if (ratio[1]) {
+            this.ratio = ratio[1].split('x')[0] / ratio[1].split('x')[1];
+          }
+        }
+      }
+      this.isRendering = true;
       this.update();
     }
+  }
+
+  render () {
+    const width = this.getRect().width;
+    if (width !== this.currentWidth) this.update();
+  }
+
+  update () {
+    this.currentWidth = this.getRect().width;
+    console.log(this.currentWidth, this.ratio, this.currentWidth / this.ratio + 'px');
+    console.log(this.style.height);
+
+    this.style.height = this.currentWidth / this.ratio + 'px';
+    console.log(this.style.height);
   }
 }
 
