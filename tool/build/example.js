e071de9f17e20110b20ef8180452ca29e35190fb
lab9 <lab9.fr@gmail.com>
2022-05-20 19:26:25 +0200

    refactor(global): implémentation du positionnement des icônes en inline [DS-2526] (#320)

--- tool/build/example.js
+++ tool/build/example.js
@@ -9,9 +9,8 @@ const { getPackages } = require('../utilities/config');
 const beautyOpts = beautify.defaultOptions();
 beautyOpts.end_with_newline = true;
 beautyOpts.max_preserve_newlines = 0;
-beautyOpts.indent_inner_html = true;
+beautyOpts.indent_inner_html = false;
 beautyOpts.indent_handlebars = true;
-
 beautyOpts.inline = [];
 
 let count = 0;
