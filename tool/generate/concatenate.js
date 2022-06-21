45d566b9613d4d799e6b109f8fa4868d8a56552c
lab9 <lab9.fr@gmail.com>
2022-06-21 18:56:16 +0200

    chore(global): refactor path et media query

--- tool/generate/concatenate.js
+++ tool/generate/concatenate.js
@@ -17,13 +17,8 @@ const generateStyle = (pck, file) => {
   const ascendance = `${pck.path}/`;
 
   const depthLevel = (ascendance.match(/\//g) || []).length - 1;
-  let pathToCore = '';
-  for (let i = 0; i < depthLevel; i++) {
-    pathToCore += '../';
-  }
-  content += `@import '${pathToCore}media-query-order';\n`;
 
-  content += `@import '${pathToCore}core/style/path/path-${depthLevel}';\n`;
+  content += `@use 'module/dsfr';\n@include dsfr.set-dist-path-level(${depthLevel});\n@include dsfr.media-query-order;\n\n`;
 
   for (const child of children) {
     content += `@import '${child.path.replace(ascendance, '')}/${file}';\n`;
