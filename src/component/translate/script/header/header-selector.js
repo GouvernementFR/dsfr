216ef728e1dfc0d0f8042786048753c16016cac9
keryan <keryan.sanie@gmail.com>
2022-06-22 20:33:51 +0200

    feat(translate): ajout translate et correction nav/header

---
+++ src/component/translate/script/header/header-selector.js
@@ -0,0 +1,9 @@
+import api from '../../api.js';
+
+export const HeaderSelector = {
+  HEADER: api.internals.ns.selector('header'),
+  TOOLS_LINKS: api.internals.ns.selector('header__tools-links'),
+  MENU_LINKS: api.internals.ns.selector('header__menu-links'),
+  BUTTONS: `${api.internals.ns.selector('header__tools-links')} ${api.internals.ns.selector('btns-group')}, ${api.internals.ns.selector('header__tools-links')} ${api.internals.ns.selector('links-group')}`,
+  MODALS: `${api.internals.ns.selector('header__search')}${api.internals.ns.selector('modal')}, ${api.internals.ns.selector('header__menu')}${api.internals.ns.selector('modal')}`
+};
