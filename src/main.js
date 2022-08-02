f378616290637d95412372b4ee31e13fc654c1b9
lab9 <lab9.fr@gmail.com>
2022-08-02 20:45:59 +0200

    refactor(global): base

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import api from './core/main.js';
 import './legacy/main.js';
-import './utility/scheme/_content/script/main.js';
+import './scheme/main.js';
 import './component/main.js';
 export default api;
