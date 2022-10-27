e7b1ee5ac79a2557b40c630df27c13b997837ea3
lab9 <lab9.fr@gmail.com>
2022-10-27 11:50:17 +0200

    chore: maj 1.8.0 H

---
+++ src/dsfr/pattern/name/_content/example/data/js/add-firstname.js
@@ -0,0 +1,17 @@
+if (this.firstnamesCount === undefined) this.firstnamesCount = 1;
+else this.firstnamesCount++;
+
+const getFieldsetElement = (node) => {
+  const parent = node.parentNode;
+  if (parent.className.indexOf('<%= prefix %>-fieldset__element') > -1) return parent;
+  return getFieldsetElement(parent);
+};
+
+const firstname = getFieldsetElement(document.getElementById('<%= id %>'));
+
+const reference = getFieldsetElement(this);
+
+const copy = firstname.cloneNode(true);
+copy.innerHTML = copy.innerHTML.replace(/<%= id %>/g, `<%= id %>-added-${this.firstnamesCount}`).replace('name=&quot;firstname&quot;', `name=&quot;firstname-${this.firstnamesCount}&quot;`);
+const container = reference.parentNode;
+container.insertBefore(copy, reference);
