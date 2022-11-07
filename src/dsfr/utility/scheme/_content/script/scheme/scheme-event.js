13d1a442bbaa569c035ae5a7dfe8a89ed743c330
lab9 <lab9.fr@gmail.com>
2022-11-07 12:28:07 +0100

    chore: maj 1.8.0 I

--- src/dsfr/utility/scheme/_content/script/scheme/scheme-event.js
+++ src/dsfr/utility/scheme/_content/script/scheme/scheme-event.js
@@ -1,8 +1,8 @@
 import ref from '../../../ref.js';
 
 const SchemeEvent = {
-  SCHEME: ref.internals.event('scheme'),
-  THEME: ref.internals.event('theme')
+  SCHEME: ref.internals.ns.event('scheme'),
+  THEME: ref.internals.ns.event('theme')
 };
 
 export { SchemeEvent };
