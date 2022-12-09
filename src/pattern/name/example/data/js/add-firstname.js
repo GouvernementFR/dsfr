8da5934b5f664e6648656809562970d1650a6e5d
keryan <keryan.sanie@gmail.com>
2022-12-09 19:17:16 +0100

    feat: ajout bouton de suppression de prenom

--- src/pattern/name/example/data/js/add-firstname.js
+++ src/pattern/name/example/data/js/add-firstname.js
@@ -12,6 +12,19 @@ const firstname = getFieldsetElement(document.getElementById('<%= id %>'));
 const reference = getFieldsetElement(this);
 
 const copy = firstname.cloneNode(true);
+copy.classList.add('<%= prefix %>-fieldset__element--inline', '<%= prefix %>-fieldset__element--inline-grow');
 copy.innerHTML = copy.innerHTML.replace(/<%= id %>/g, `<%= id %>-added-${this.firstnamesCount}`).replace('name=&quot;given-name&quot;', `name=&quot;additional-name-${this.firstnamesCount}&quot;`).replace('autocomplete=&quot;given-name&quot;', 'autocomplete=&quot;additional-name&quot;');
+
+const removeField = reference.cloneNode(true);
+removeField.classList.add('<%= prefix %>-fieldset__element--inline');
+
+const removeButton = removeField.getElementsByTagName('button')[0];
+const labelRemoveButton = 'Supprimer le prénom';
+removeButton.classList = '<%= prefix %>-btn <%= prefix %>-btn--tertiary <%= prefix %>-icon-delete-line';
+removeButton.innerHTML = labelRemoveButton;
+removeButton.title = labelRemoveButton;
+removeButton.setAttribute('onclick', `document.getElementById('<%= id %>-added-${this.firstnamesCount}').closest('.<%= prefix %>-fieldset__element').remove(); this.closest('.<%= prefix %>-fieldset__element').remove()`);
+
 const container = reference.parentNode;
-container.insertBefore(copy, reference);
+container.insertBefore(removeField, reference);
+container.insertBefore(copy, removeField);
