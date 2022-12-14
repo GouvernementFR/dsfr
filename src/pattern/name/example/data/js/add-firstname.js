c17d1ca9b7246f34dd7df1e9f773bba14781e5ff
keryan <keryan.sanie@gmail.com>
2022-12-14 18:39:31 +0100

    feat(input, name): input-action

--- src/pattern/name/example/data/js/add-firstname.js
+++ src/pattern/name/example/data/js/add-firstname.js
@@ -8,23 +8,23 @@ const getFieldsetElement = (node) => {
 };
 
 const firstname = getFieldsetElement(document.getElementById('<%= id %>'));
-
 const reference = getFieldsetElement(this);
 
 const copy = firstname.cloneNode(true);
-copy.classList.add('<%= prefix %>-fieldset__element--inline', '<%= prefix %>-fieldset__element--inline-grow');
 copy.innerHTML = copy.innerHTML.replace(/<%= id %>/g, `<%= id %>-added-${this.firstnamesCount}`).replace('name=&quot;given-name&quot;', `name=&quot;additional-name-${this.firstnamesCount}&quot;`).replace('autocomplete=&quot;given-name&quot;', 'autocomplete=&quot;additional-name&quot;');
 
-const removeField = reference.cloneNode(true);
-removeField.classList.add('<%= prefix %>-fieldset__element--inline');
+const wrapper = document.createElement('div');
+wrapper.classList = '<%= prefix %>-input-wrap <%= prefix %>-input-wrap--action';
 
-const removeButton = removeField.getElementsByTagName('button')[0];
+const removeButton = document.createElement('button');
 const labelRemoveButton = '<%= getText("label.remove-firstname", "name") %>';
 removeButton.classList = '<%= prefix %>-btn <%= prefix %>-btn--tertiary <%= prefix %>-icon-delete-line';
 removeButton.innerHTML = labelRemoveButton;
 removeButton.title = labelRemoveButton;
 removeButton.setAttribute('onclick', `document.getElementById('<%= id %>-added-${this.firstnamesCount}').closest('.<%= prefix %>-fieldset__element').remove(); this.closest('.<%= prefix %>-fieldset__element').remove()`);
 
-const container = reference.parentNode;
-container.insertBefore(removeField, reference);
-container.insertBefore(copy, removeField);
+wrapper.appendChild(copy.getElementsByTagName('input')[0]);
+wrapper.appendChild(removeButton);
+copy.getElementsByTagName('label')[0].after(wrapper);
+reference.parentNode.insertBefore(copy, reference);
+document.getElementById(`<%= id %>-added-${this.firstnamesCount}`).focus();
