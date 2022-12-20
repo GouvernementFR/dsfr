b23ab4bfbd553f3f0b9d8dd40554a05f3cabfb8d
zellerbaptiste <zellerbaptiste@gmail.com>
2022-12-20 14:33:15 +0100

    feat(tooltip): corrige erreur de lint et met a jour le wording

--- src/core/script/placement/place-selector.js
+++ src/core/script/placement/place-selector.js
@@ -4,5 +4,5 @@ export const PlaceSelector = {
   TOP: ns.selector('placement--top'),
   RIGHT: ns.selector('placement--right'),
   BOTTOM: ns.selector('placement--bottom'),
-  LEFT: ns.selector('placement--left'),
+  LEFT: ns.selector('placement--left')
 };
