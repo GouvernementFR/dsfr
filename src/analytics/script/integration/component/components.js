3f2dfddc34b97e674b1a17edf3a69e1f6a573c3f
zellerbaptiste <zellerbaptiste@gmail.com>
2023-02-21 11:21:57 +0100

    feat(analytics): component tab

--- src/analytics/script/integration/component/components.js
+++ src/analytics/script/integration/component/components.js
@@ -1,6 +1,7 @@
 import api from '../../../api';
 import { AccordionActionee } from './accordion/accordion-actionee';
 import { ModalActionee } from './modal/modal-actionee';
+import { TabActionee } from './tab/tab-actionee';
 
 if (api.accordion) {
   api.internals.register(api.accordion.AccordionSelector.COLLAPSE, AccordionActionee);
@@ -8,6 +9,10 @@ if (api.accordion) {
 
 if (api.modal) {
   api.internals.register(api.modal.ModalSelector.MODAL, ModalActionee);
+}
+
+if (api.tab) {
+  api.internals.register(api.tab.TabSelector.PANEL, TabActionee);
 }
 
 export default api;
---
+++ src/analytics/script/integration/component/tab/tab-actionee.js
@@ -0,0 +1,38 @@
+import api from '../../../../api.js';
+import { ComponentActionee } from '../component-actionee';
+import { ActionElement } from '../../../analytics/action/action-element';
+import { Type } from '../../../analytics/action/type';
+import { TabButtonActionee } from './tab-button-actionee';
+import ID from './id';
+
+class TabActionee extends ComponentActionee {
+  static get instanceClassName () {
+    return 'TabActionee';
+  }
+
+  init () {
+    this.register(`[aria-controls="${this.id}"]`, TabButtonActionee);
+    this._actionElement = new ActionElement(this.node, Type.DISCLOSE);
+    this.listen(api.core.DisclosureEvent.DISCLOSE, this.handleDisclose.bind(this));
+  }
+
+  handleDisclose () {
+    this._actionElement.act();
+  }
+
+  get label () {
+    const selector = Array.from({ length: 6 }, (v, i) => `h${i + 1}`).join(',');
+    const headings = [this.node.querySelector(selector)].filter(heading => (this.node.compareDocumentPosition(heading) & Node.DOCUMENT_POSITION_CONTAINED_BY) > 0);
+
+    const button = document.querySelector(`[aria-controls="${this.id}"]`);
+
+    if (!headings.length || !button) return null;
+    return headings[0].textContent.trim() || button.textContent.trim();
+  }
+
+  get component () {
+    return ID;
+  }
+}
+
+export { TabActionee };
