75efd04b20166457f0296ef1111466699aa30c56
lab9 <lab9.fr@gmail.com>
2023-03-20 17:08:07 +0100

    fix(analytics): various

--- src/analytics/script/integration/component/button/button-selector.js
+++ src/analytics/script/integration/component/button/button-selector.js
@@ -1,5 +1,5 @@
 import api from '../../../../api.js';
 
 export const ButtonSelector = {
-  BUTTON: api.internals.ns.selector('btn')
+  BUTTON: `${api.internals.ns.selector('btn')}:not(${api.internals.ns.selector('btn--close')})`
 };
