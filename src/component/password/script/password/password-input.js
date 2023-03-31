58dcaa78084e5148cb5a0508ae8258931706f1aa
lab9 <lab9.fr@gmail.com>
2023-03-31 20:09:52 +0200

    fix(password): support optional chaining trop récent

--- src/component/password/script/password/password-input.js
+++ src/component/password/script/password/password-input.js
@@ -23,9 +23,7 @@ class PasswordInput extends api.core.Instance {
   }
 
   capslock (event) {
-    if (typeof event?.getModifierState !== 'function') {
-      return;
-    }
+    if (event && typeof event.getModifierState !== 'function') return;
     if (event.getModifierState('CapsLock')) {
       this.node.parentNode.setAttribute(api.internals.ns.attr('capslock'), '');
     } else {
