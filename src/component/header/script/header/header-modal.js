a107284b1ff36c7f45876fbaff1294b09794694d
lab9 <lab9.fr@gmail.com>
2023-04-11 11:38:44 +0200

    feat(header,navigation): fermeture de la navigation au clic sur lien ou bouton

--- src/component/header/script/header/header-modal.js
+++ src/component/header/script/header/header-modal.js
@@ -1,6 +1,11 @@
 import api from '../../api.js';
 
 class HeaderModal extends api.core.Instance {
+  constructor () {
+    super();
+    this._mouseUpHandling = this.mouseUpHandler.bind(this);
+  }
+
   static get instanceClassName () {
     return 'HeaderModal';
   }
@@ -25,6 +30,7 @@ class HeaderModal extends api.core.Instance {
       if (button.isPrimary && id) break;
     }
     this.setAttribute('aria-labelledby', id);
+    this.listen('mouseup', this._mouseUpHandling, { capture: true });
   }
 
   unqualify () {
@@ -32,6 +38,14 @@ class HeaderModal extends api.core.Instance {
     if (modal) modal.conceal();
     this.removeAttribute('role');
     this.removeAttribute('aria-labelledby');
+    this.unlisten('mouseup', this._mouseUpHandling, { capture: true });
+  }
+
+  mouseUpHandler (e) {
+    if (e.target.matches('a, button') && !e.target.matches('[aria-controls]') && !e.target.matches(api.core.DisclosureSelector.PREVENT_CONCEAL)) {
+      const modal = this.element.getInstance('Modal');
+      modal.conceal();
+    }
   }
 }
 
