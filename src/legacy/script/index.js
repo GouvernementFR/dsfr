cf4086ca431db2411eda288d3eb81885db5fc960
lab9 <lab9.fr@gmail.com>
2023-04-12 12:52:59 +0200

    feat(legacy): array.from & array.prototype.flat

--- src/legacy/script/index.js
+++ src/legacy/script/index.js
@@ -6,6 +6,8 @@ import './polyfill-io/foreach-polyfill.min';
 import './polyfill-io/contains-polyfill.min';
 import './polyfill-io/matches-polyfill.min';
 import './polyfill-io/promise-polyfill.min';
+import './polyfill-io/array-from-polyfill.min';
+import './polyfill-io/array-prototype-flat-polyfill.min';
 import './polyfill-io/object-get-own-property-symbols-polyfill.min';
 import './polyfill-io/object-values-polyfill.min';
 
