e1bb5979c6c96b61dcddb2dd144213df8318ffb9
zellerbaptiste <zellerbaptiste@gmail.com>
2023-07-05 16:43:36 +0200

    fix(pattern-name): a11y - ajoute le focus sur le bouton ajouter

--- src/pattern/name/example/js/remove-firstname.js
+++ src/pattern/name/example/js/remove-firstname.js
@@ -1,12 +1,4 @@
 window.removeFirstname = (copy) => {
-  const attrGivenName = '[name=given-name]';
-  const fieldsetElements = copy.parentNode.querySelectorAll('.fr-fieldset__element');
-  if (copy.nextSibling.querySelector(attrGivenName)) {
-    copy.nextSibling.querySelector(attrGivenName).focus();
-  } else {
-    if (fieldsetElements.length) {
-      fieldsetElements[fieldsetElements.length - 1].querySelector('button').focus();
-    }
-  }
+  copy.nextSibling.querySelector('input,button').focus();
   copy.remove();
 };
