ff3d9790fd80655d7a30946e3ed4461835481629
lab9 <lab9.fr@gmail.com>
2023-11-08 16:34:36 +0100

    fix(analytics): ajout des sélecteurs card + tile avec bouton

--- src/analytics/script/integration/component/card/card-selector.js
+++ src/analytics/script/integration/component/card/card-selector.js
@@ -2,6 +2,6 @@ import api from '../../../../api';
 
 export const CardSelector = {
   CARD: api.internals.ns.selector('card'),
-  LINK: `${api.internals.ns.selector('card__title')} a`,
+  LINK: `${api.internals.ns.selector('card__title')} a, ${api.internals.ns.selector('card__title')} button`,
   TITLE: api.internals.ns.selector('card__title')
 };
