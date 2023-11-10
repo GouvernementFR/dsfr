753ad8f43ee8ebf6678f52e96a458795bb045ad0
lab9 <lab9.fr@gmail.com>
2023-11-10 14:20:56 +0100

    Update src/component/range/script/range/range-emission.js
    Co-authored-by: Baptiste.z <zellerbaptiste@gmail.com>

--- src/component/range/script/range/range-emission.js
+++ src/component/range/script/range/range-emission.js
@@ -5,7 +5,7 @@ export const RangeEmission = {
   CLEAR: api.internals.ns.emission('range', 'clear'),
   VALUE: api.internals.ns.emission('range', 'value'),
   VALUE2: api.internals.ns.emission('range', 'value2'),
-  CONSTRAINTS: api.internals.ns.emission('range', 'contraints'),
+  CONSTRAINTS: api.internals.ns.emission('range', 'constraints'),
   MAX: api.internals.ns.emission('range', 'max'),
   STEP: api.internals.ns.emission('range', 'step'),
   PREFIX: api.internals.ns.emission('range', 'prefix'),
