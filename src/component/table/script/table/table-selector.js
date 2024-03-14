a191822b0d0be66ed97ec721873adcafb63931ee
zellerbaptiste <zellerbaptiste@gmail.com>
2024-03-14 09:49:02 +0100

    feat(table): ajoute les entetes de ligne

--- src/component/table/script/table/table-selector.js
+++ src/component/table/script/table/table-selector.js
@@ -5,6 +5,6 @@ export const TableSelector = {
   SHADOW: api.internals.ns.selector('table__shadow'),
   SHADOW_LEFT: api.internals.ns.selector('table__shadow--left'),
   SHADOW_RIGHT: api.internals.ns.selector('table__shadow--right'),
-  ELEMENT: `${api.internals.ns.selector('table')}:not(${api.internals.ns.selector('table--no-scroll')}) table`,
+  ELEMENT: [`${api.internals.ns.selector('table')}:not(${api.internals.ns.selector('table--no-scroll')}) table`, `${api.internals.ns.selector('table')} ${api.internals.ns.selector('table__container')}`],
   CAPTION: `${api.internals.ns.selector('table')} table caption`
 };
