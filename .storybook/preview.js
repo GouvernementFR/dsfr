ad719fa03f9a1a324844f11f0fba621e92e68523
lab9 <lab9.fr@gmail.com>
2024-06-08 10:59:32 +0200

    feat: ajout dark mode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,44 +6,37 @@ import '../dist/utility/utility.css';
 import '../dist/core/core.module.js';
 import '../dist/component/component.module.js';
 import dsfrTheme from './dsfr-theme';
-import { withThemeByDataAttribute, DecoratorHelpers  } from '@storybook/addon-themes';
+import { DecoratorHelpers } from '@storybook/addon-themes';
+import { UrlStore } from '@storybook/preview-api';
 
-const { initializeThemeState, pluckThemeFromContext, useThemeParameters,  } = DecoratorHelpers;
+const { initializeThemeState, pluckThemeFromContext, useThemeParameters } = DecoratorHelpers;
 
-let currentTheme;
+const defaultTheme = 'light';
+const themes = Object.keys(dsfrTheme);
+initializeThemeState(themes, defaultTheme);
 
-const getThemeDecorator = () => {
-  const defaultTheme = 'light';
-  initializeThemeState(Object.keys[dsfrTheme], defaultTheme);
-  return (Story, context) => {
-    const selectedTheme = pluckThemeFromContext(context);
-    const { themeOverride } = useThemeParameters();
+const themeDecorator = (Story, context) => {
+  const selectedTheme = pluckThemeFromContext(context);
+  const { themeOverride } = useThemeParameters();
 
-    currentTheme = themeOverride || selectedTheme || defaultTheme;
+  const theme = themeOverride || selectedTheme || defaultTheme;
 
-    context.parameters.docs.theme = dsfrTheme[currentTheme];
+  document.documentElement.setAttribute('data-fr-theme', theme);
+  context.parameters.docs.theme = dsfrTheme[theme];
 
-    return Story();
-  };
+  return Story();
 };
 
 const getInitialTheme = () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const globals = (urlParams.get('globals') ?? '').split(';').reduce((acc, item) => { const pair = item.split(':'); acc[pair[0]] = pair[1]; return acc; }, {});
-  return dsfrTheme[globals.theme || 'light'];
+  const store = new UrlStore();
+  const theme = store?.selectionSpecifier?.globals?.theme ?? 'light';
+  document.documentElement.setAttribute('data-fr-theme', theme);
+  return dsfrTheme[theme];
 }
 
 const preview = {
   decorators: [
-    getThemeDecorator(),
-    withThemeByDataAttribute({
-      themes: {
-        light: 'light',
-        dark: 'dark'
-      },
-      defaultTheme: 'light',
-      attributeName: 'data-fr-scheme',
-    })
+    themeDecorator,
   ],
   parameters: {
     backgrounds: {
