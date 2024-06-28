7f642e9f1d8196c3be5c508b193b1bcd40413fe1
Keryan SANIE <keryan.sanie@gmail.com>
2024-06-28 17:56:16 +0200

    feat(core): storybook core renderer

--- src/renderer.js
+++ src/renderer.js
@@ -1,11 +1,13 @@
 import { EJSRenderer } from './core/template/stories/ejs-renderer';
-import { badgePartials } from './component/badge/template/stories/partials'
+import { corePartials } from './core/template/stories/partials';
+import { badgePartials } from './component/badge/template/stories/partials';
 import { buttonPartials } from './component/button/template/stories/partials';
 import { linkPartials } from './component/link/template/stories/partials';
 import { tooltipPartials } from './component/tooltip/template/stories/partials';
 
 const ejsRenderer = new EJSRenderer();
 
+corePartials.forEach(({ names, partial }) => ejsRenderer.add(names, partial));
 badgePartials.forEach(({ names, partial }) => ejsRenderer.add(names, partial));
 buttonPartials.forEach(({ names, partial }) => ejsRenderer.add(names, partial));
 linkPartials.forEach(({ names, partial }) => ejsRenderer.add(names, partial));
