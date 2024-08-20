7eff8db3ee45e16a1f8646796b76bdaf4f2ebb52
Keryan SANIE <keryan.sanie@gmail.com>
2024-08-20 17:26:03 +0200

    feat(logo, connect): storybook

---
+++ src/component/logo/template/stories/logo.js
@@ -0,0 +1,4 @@
+import ejsRenderer from '../../../../renderer.js';
+const renderLogo = (data) => ejsRenderer.render('logo', data);
+
+export { renderLogo };
---
+++ src/component/logo/template/stories/logo.stories.js
@@ -0,0 +1,43 @@
+import { renderLogo } from './logo';
+import { logoArgs, logoArgTypes, logoProps } from './logo-arg-types';
+
+const render = (args) => renderLogo({ logo: logoProps(args) });
+
+export default {
+  id: 'logo',
+  title: 'DSFR/Component/Logo',
+  render: render,
+  argTypes: logoArgTypes,
+  args: logoArgs
+};
+
+export const ControlsStory = {
+  tags: ['!dev'],
+  args: {}
+};
+
+export const DefaultStory = {
+  // tags: ['!dev'],
+  args: {}
+};
+
+export const SmStory = {
+  tags: ['!dev'],
+  args: {
+    size: 'sm'
+  }
+};
+
+export const MdStory = {
+  tags: ['!dev'],
+  args: {
+    size: 'md'
+  }
+};
+
+export const LgStory = {
+  tags: ['!dev'],
+  args: {
+    size: 'sm'
+  }
+};
