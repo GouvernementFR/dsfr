ffbcac32d141fd134a05379945c79e080870127c
Keryan SANIE <keryan.sanie@gmail.com>
2024-08-26 17:31:53 +0200

    feat(transcription): storybook

---
+++ src/component/transcription/template/stories/transcription.stories.js
@@ -0,0 +1,22 @@
+import { renderTranscription } from './transcription';
+import { transcriptionArgs, transcriptionArgTypes, transcriptionProps } from './transcription-arg-types';
+
+const render = (args) => renderTranscription({ transcription: { ...transcriptionProps(args), attributes: { style: 'margin-bottom: 300px' } } });
+
+export default {
+  id: 'transcription',
+  title: 'DSFR/Component/Transcription',
+  render: render,
+  argTypes: transcriptionArgTypes,
+  args: transcriptionArgs
+};
+
+export const ControlsStory = {
+  tags: ['!dev'],
+  args: {}
+};
+
+export const DefaultStory = {
+  // tags: ['!dev'],
+  args: {}
+};
