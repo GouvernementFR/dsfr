b54fb71b5ea318c591ed8cfc31e04b1f893c0a91
Keryan SANIE <keryan.sanie@gmail.com>
2024-08-26 17:10:49 +0200

    feat(modal): storybook

---
+++ src/component/modal/template/stories/partials.js
@@ -0,0 +1,13 @@
+import button from '../ejs/button.ejs?raw';
+import modal from '../ejs/modal.ejs?raw';
+
+export const modalPartials = [
+  {
+    names: ['buttonModal'],
+    partial: button
+  },
+  {
+    names: ['modal'],
+    partial: modal
+  }
+];
