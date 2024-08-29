335e3e21c2e095460ed2a3527ac703936e48365a
Keryan SANIE <keryan.sanie@gmail.com>
2024-08-29 10:45:59 +0200

    feat(form, checkbox): storybook

---
+++ src/component/form/template/stories/form.stories.js
@@ -0,0 +1,22 @@
+import { renderForm } from './form';
+import { formArgs, formArgTypes, formProps } from './form-arg-types';
+
+const render = (args) => renderForm({ form: formProps(args) });
+
+export default {
+  id: 'form',
+  title: 'DSFR/Component/Form',
+  render: render,
+  argTypes: formArgTypes,
+  args: formArgs
+};
+
+export const ControlsStory = {
+  tags: ['!dev'],
+  args: {}
+};
+
+export const DefaultStory = {
+  // tags: ['!dev'],
+  args: {}
+};
