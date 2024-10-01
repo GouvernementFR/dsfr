094b62b8d19f9275bc71ae6e16c48e57184956c7
Keryan SANIE <keryan.sanie@gmail.com>
2024-10-01 15:57:28 +0200

    feat(storybook): footer

---
+++ src/component/footer/template/stories/footer.stories.js
@@ -0,0 +1,22 @@
+import { footerArgs, footerArgTypes, footerProps } from './footer-arg-types';
+import { renderFooter } from './footer';
+
+const render = (args) => renderFooter({ footer: footerProps(args) });
+
+export default {
+  id: 'footer',
+  title: 'DSFR/Component/Footer',
+  render: render,
+  argTypes: footerArgTypes,
+  args: footerArgs
+};
+
+export const ControlsStory = {
+  tags: ['!dev'],
+  args: {}
+};
+
+export const DefaultStory = {
+  // tags: ['!dev'],
+  args: {}
+};
