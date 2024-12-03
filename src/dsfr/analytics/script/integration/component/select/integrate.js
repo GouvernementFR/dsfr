5d439776dbbb904fe22bc0fbeb6c0c8b71cac692
zellerbaptiste <zellerbaptiste@gmail.com>
2024-12-03 10:36:59 +0100

    fix(lint): corrige le build

--- src/dsfr/analytics/script/integration/component/select/integrate.js
+++ src/dsfr/analytics/script/integration/component/select/integrate.js
@@ -1,7 +1,7 @@
 import api from '../../../../api';
 import { SelectSelector } from './select-selector';
 import { SelectActionee } from './select-actionee';
-import { joinSelector } from '../../join-selector'
+import { joinSelector } from '../../join-selector';
 
 const integrateSelect = (selector = '') => {
   api.internals.register(joinSelector(SelectSelector.SELECT, selector), SelectActionee);
