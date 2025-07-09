0b4ca24d56332ed4180835ed3d4257dba40e139c
MatRouillard <mathis.rouillard@ign.fr>
2025-07-09 11:13:37 +0200

    feat: Ajout fonctionnalités composant Access et Header

--- src/dsfr/component/access/main.js
+++
@@ -1,6 +0,0 @@
-import api from './index.js';
-
-api.internals.register(api.access.AccessSelector.TOOLS_LINKS, api.access.AccessLinks);
-api.internals.register(api.access.AccessSelector.MODALS, api.access.AccessModal);
-
-export default api;
