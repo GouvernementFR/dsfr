3cb0eba7fc8ad2d04301a7e802fff10de0a243d1
Keryan SANIE <keryan.sanie@gmail.com>
2026-02-24 12:02:51 +0100

    fix: ajout de scroll-padding-bottom

--- src/dsfr/component/modal/main.js
+++ src/dsfr/component/modal/main.js
@@ -2,6 +2,7 @@ import api from './index.js';
 
 api.internals.register(api.modal.ModalSelector.MODAL, api.modal.Modal);
 api.internals.register(api.modal.ModalSelector.BODY, api.modal.ModalBody);
+api.internals.register(api.modal.ModalSelector.FOOTER, api.modal.ModalFooter);
 api.internals.register(api.core.RootSelector.ROOT, api.modal.ModalsGroup);
 
 export default api;
